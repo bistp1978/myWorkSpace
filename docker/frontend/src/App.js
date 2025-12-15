0622a57bf25d5eaff4b525ef180ab571d712a3e6
Prashant Bist <prashant.bist@rediffmail.com>
2025-12-15 15:01:28 +0000

    changed

--- docker/frontend/src/App.js
+++ docker/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload pressing f5 again !.
+          Edit <code>src/App.js</code> and save to reload pressing f5 again and again!.
         </p>
         <a
           className="App-link"
