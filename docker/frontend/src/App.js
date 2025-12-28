cfe217008e8b3af44f4f47d324c31a07819acad3
Prashant Bist <prashant.bist@rediffmail.com>
2025-12-28 13:04:45 +0530

    volume mounts

--- docker/frontend/src/App.js
+++ docker/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload pressing f5 again and again!.
+          Edit <code>src/App.js</code> and save to reload pressing f5 .
         </p>
         <a
           className="App-link"
